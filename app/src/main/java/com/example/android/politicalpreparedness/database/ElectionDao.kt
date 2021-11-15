aea97ed1d83002374df29f917df769e04b1386c5
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-11-15 10:29:11 -0500

    Update ElectionListAdapter

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,22 +1,24 @@
 package com.example.android.politicalpreparedness.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import com.example.android.politicalpreparedness.network.models.Election
 
 @Dao
 interface ElectionDao {
 
-    //TODO: Add insert query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertElections(election: List<Election>)
 
-    //TODO: Add select all election query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertElection(election: Election): Long
 
-    //TODO: Add select single election query
+    @Query("SELECT * FROM election_table")
+    fun getAllElections(): LiveData<List<Election>>
 
-    //TODO: Add delete query
+    @Query("SELECT * FROM election_table WHERE id=:id")
+    fun getElectionById(id: Int): LiveData<Election>
 
-    //TODO: Add clear query
-
+    @Query("DELETE FROM election_table where id=:id")
+    fun deleteElection(id: Int)
 }
--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,23 +1,55 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ElectionItemBinding
 import com.example.android.politicalpreparedness.network.models.Election
+import java.nio.file.attribute.FileTime.from
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election,
+        ElectionViewHolder>(ElectionDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
-        return ElectionViewHolder.from(parent)
+    private var elections = listOf<Election>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+    override fun getItemCount(): Int = elections.size
+
+    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.election = elections[position]
+            it.clickListener = clickListener
+        }
     }
 
-    //TODO: Bind ViewHolder
-
-    //TODO: Add companion object to inflate ViewHolder (from)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
+        val binding = ElectionItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return ElectionViewHolder(binding)
+    }
 }
 
-//TODO: Create ElectionViewHolder
+// Create ElectionViewHolder
+class ElectionViewHolder(val viewDataBinding: ElectionItemBinding) :
+    RecyclerView.ViewHolder(viewDataBinding.root) {
+}
 
-//TODO: Create ElectionDiffCallback
+// Create ElectionDiffCallback
+class ElectionDiffCallback() : DiffUtil.ItemCallback<Election>() {
+    override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem.id == newItem.id
+    }
 
-//TODO: Create ElectionListener
+    override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem == newItem
+    }
+
+}
+// Create ElectionListener
+class ElectionListener(val block: (Election) -> Unit) {
+    fun onClick(election: Election) = block(election)
+}
