dc308f1e0d4337742d42780500e84291582d926c
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-11-18 12:27:07 -0500

    Add VoterInfoViewModel

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,32 +1,69 @@
 package com.example.android.politicalpreparedness.election
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
 
 class VoterInfoFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
 
-        //TODO: Add ViewModel values and create ViewModel
+    private val args: VoterInfoFragmentArgs by navArgs()
 
-        //TODO: Add binding values
+    //Add ViewModel values and create ViewModel
+    private val viewModel: VoterInfoViewModel by lazy {
 
-        //TODO: Populate voter info -- hide views without provided data.
-        /**
-        Hint: You will need to ensure proper data is provided from previous fragment.
-        */
-
-
-        //TODO: Handle loading of URLs
-
-        //TODO: Handle save button UI state
-        //TODO: cont'd Handle save button clicks
-
+        val application = requireNotNull(this.activity).application
+        val viewModelFactory = VoterInfoViewModelFactory(application)
+        ViewModelProvider(this, viewModelFactory)
+            .get(VoterInfoViewModel::class.java)
     }
 
-    //TODO: Create method to load URL intents
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        //Add binding values
+        val binding: FragmentVoterInfoBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_voter_info,
+            container,
+            false)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        //Populate voter info -- hide views without provided data.
+        val election = args.argElection
+
+        viewModel.getElection(election.id)
+
+        if (election.division.state.isEmpty()) {
+            viewModel.getVoterInfo(
+                args.argElection.id,
+                args.argElection.division.country)
+        } else {
+            viewModel.getVoterInfo(
+                args.argElection.id,
+                "${args.argElection.division.country} - ${args.argElection.division.state}")
+        }
+
+        //Create method to load URL intents
+        viewModel.url.observe(viewLifecycleOwner, Observer {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+            startActivity(intent)
+        })
+
+        return binding.root
+    }
+
 
 }
